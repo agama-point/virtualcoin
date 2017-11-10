fc71c873fcd29e22c2704eda7a2145e243510493
Honza S. Copak <bonum.comunae@volny.cz>
2017-11-10 16:53:07 +0100

    Update myWallets.py

--- oeVirtualCoin/myWallets.py
+++ oeVirtualCoin/myWallets.py
@@ -1,8 +1,8 @@
 WLTC="LNTKxPMWDNAHT1rUkAGmS81CMBHqB3W723"
 WBTC="11r118H2Qv4oHfjFuJnuU8GZHGNqwEH9e"
+PBTC="5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreBF8or94" 
+#test pk http://directory.io/ >  1E1NUNmYw1G5c3FKNPd435QmDvuNG3auYk
 
-def getWBTC():
-	return WBTC
-	
-def getWLTC():
-	return WLTC	
+def getWBTC():	return WBTC
+def getPBTC():	return PBTC	
+def getWLTC():	return WLTC	
