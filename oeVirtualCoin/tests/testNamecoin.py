81d0b329b49a844b65338305f1136db994a330b9
Honza S. Copak <bonum.comunae@volny.cz>
2017-11-11 08:59:47 +0100

    Update testNamecoin.py

--- oeVirtualCoin/tests/testNamecoin.py
+++ oeVirtualCoin/tests/testNamecoin.py
@@ -11,3 +11,5 @@ namecoin_public_key = namecoin_private_key.public_key()
 ncaddr = namecoin_public_key.address()
 #'MyMFt8fQdZ6rEyDhZbe2vd19gD8gzagr7Z'
 print ncaddr
+
+#explorer https://bchain.info/NMC/addr/N83EcwrAMDWicACS7u42E1X8x8d4vua1Av
