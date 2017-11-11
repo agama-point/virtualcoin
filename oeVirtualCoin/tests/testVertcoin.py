2451e51736282a2052a467068d60a778b566b0d6
Honza S. Copak <bonum.comunae@volny.cz>
2017-11-11 09:33:35 +0100

    Add files via upload

---
+++ oeVirtualCoin/tests/testVertcoin.py
@@ -0,0 +1,13 @@
+from pybitcoin import BitcoinPrivateKey
+
+class VertcoinPrivateKey(BitcoinPrivateKey):
+    _pubkeyhash_version_byte = 72
+    
+private_key = BitcoinPrivateKey()
+vertcoin_private_key = VertcoinPrivateKey(private_key.to_hex())
+vcpkwif = vertcoin_private_key.to_wif()
+print lcpkwif 
+
+vertcoin_public_key = vertcoin_private_key.public_key()
+vcaddr = vertcoin_public_key.address()
+print "VTC.addr: "+vcaddr
