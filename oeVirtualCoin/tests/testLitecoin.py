457f86561e9a3987342f4e027f4312ab9022a239
Honza S. Copak <bonum.comunae@volny.cz>
2017-11-11 09:16:43 +0100

    Update testLitecoin.py

--- oeVirtualCoin/tests/testLitecoin.py
+++ oeVirtualCoin/tests/testLitecoin.py
@@ -1,4 +1,4 @@
-rom pybitcoin import BitcoinPrivateKey
+from pybitcoin import BitcoinPrivateKey
 
 class LitecoinPrivateKey(BitcoinPrivateKey):
     _pubkeyhash_version_byte = 48
@@ -7,6 +7,6 @@ litcoin_private_key = LitecoinPrivateKey(private_key.to_hex())
 lcpkwif = namecoin_private_key.to_wif()
 print lcpkwif 
 
-litecoin_public_key = namecoin_private_key.public_key()
+litecoin_public_key = litecoin_private_key.public_key()
 lcaddr = litcoin_public_key.address()
-print lcaddr
+print "LTC.addr: "+lcaddr
