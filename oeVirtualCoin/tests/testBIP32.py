54333b5838bb8d07a41dbb2544469e7db7ed47b4
Honza S. Copak <bonum.comunae@volny.cz>
2017-11-11 09:26:25 +0100

    Add files via upload

---
+++ oeVirtualCoin/tests/testBIP32.py
@@ -0,0 +1,7 @@
+from pybitcoin import BitcoinPrivateKey
+
+#bip32_master_key : (seed) -> bip32 master key
+#bip32_ckd : (private or public bip32 key, i) -> child key
+#bip32_privtopub : (private bip32 key) -> public bip32 key
+#bip32_extract_key : (private or public bip32_key) -> privkey or pubkey
+
