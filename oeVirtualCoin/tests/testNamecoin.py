e6120754a0666b2cba32319aac89159307c2cf5a
Honza S. Copak <bonum.comunae@volny.cz>
2017-11-11 08:58:15 +0100

    Update testNamecoin.py

--- oeVirtualCoin/tests/testNamecoin.py
+++ oeVirtualCoin/tests/testNamecoin.py
@@ -1,11 +1,13 @@
-rom pybitcoin import BitcoinPrivateKey
+from pybitcoin import BitcoinPrivateKey
 
 class NamecoinPrivateKey(BitcoinPrivateKey):
   _pubkeyhash_version_byte = 52
 
 namecoin_private_key = NamecoinPrivateKey(private_key.to_hex())
-namecoin_private_key.to_wif()
-'73zteEjenBCK7qVtG2yRPeco2TP5w93qBW5sJkxYoGYvbWwAbXv'
+ncpkwif = namecoin_private_key.to_wif()
+print ncpkwif 
+#'73zteEjenBCK7qVtG2yRPeco2TP5w93qBW5sJkxYoGYvbWwAbXv'
 namecoin_public_key = namecoin_private_key.public_key()
-namecoin_public_key.address()
-'MyMFt8fQdZ6rEyDhZbe2vd19gD8gzagr7Z'
+ncaddr = namecoin_public_key.address()
+#'MyMFt8fQdZ6rEyDhZbe2vd19gD8gzagr7Z'
+print ncaddr
